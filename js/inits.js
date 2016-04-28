dd74b39b84c57a9d7126fb003216b8bd0145361c
thewickedwebdev <stephen.young@senioradvisor.com>
2016-04-28 17:09:38 -0500

    prettified script files

--- js/inits.js
+++ js/inits.js
@@ -1,1 +1,16 @@
-function init(){(new WOW).init(),initSchedule(),initStandings(),loadCarousel()}var scheduleContainer=$("#scheduleBody"),teamsTable=$("#teamsTable"),openMenTable=$("#openMenTable"),openWomenTable=$("#openWomenTable"),mastersMenTable=$("#mastersMenTable"),mastersWomenTable=$("#mastersWomenTable"),seniorsMenTable=$("#seniorsMenTable"),seniorsWomenTable=$("#seniorsWomenTable"),veteransMenTable=$("#veteransMenTable"),veteransWomenTable=$("#veteransWomenTable");$(function(){init()});
+function init() {
+    (new WOW).init(), initSchedule(), initStandings(), loadCarousel()
+}
+var scheduleContainer = $("#scheduleBody"),
+    teamsTable = $("#teamsTable"),
+    openMenTable = $("#openMenTable"),
+    openWomenTable = $("#openWomenTable"),
+    mastersMenTable = $("#mastersMenTable"),
+    mastersWomenTable = $("#mastersWomenTable"),
+    seniorsMenTable = $("#seniorsMenTable"),
+    seniorsWomenTable = $("#seniorsWomenTable"),
+    veteransMenTable = $("#veteransMenTable"),
+    veteransWomenTable = $("#veteransWomenTable");
+$(function() {
+    init()
+});
